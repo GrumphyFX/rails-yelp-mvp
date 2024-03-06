11c1c6809ed3d93bc20657de314a571b6fc18337
Bart Przespolewski <150512708+GrumphyFX@users.noreply.github.com>
2024-03-06 16:46:53 +0100

    rails-yelp-mvp

---
+++ db/migrate/20240306135154_add_details_to_restaurants.rb
@@ -0,0 +1,8 @@
+class AddDetailsToRestaurants < ActiveRecord::Migration[7.1]
+  def change
+    add_column :restaurants, :name, :string
+    add_column :restaurants, :address, :string
+    add_column :restaurants, :phone_number, :string
+    add_column :restaurants, :category, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,40 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+restaurants = [
+    {
+      name: "Epicure",
+      address: "75008 Paris",
+      phone_number: "01 43 54 23 31",
+      category: "french"
+    },
+    {
+      name: "La truffière",
+      address: "4 rue Blainville 75005 Paris",
+      phone_number: "01 46 33 29 82",
+      category: "french"
+    },
+    {
+      name: "Le pré catelan",
+      address: "route de Suresnes 75016 Paris",
+      phone_number: "01 44 14 41 14",
+      category: "french"
+    },
+    {
+      name: "Gaggan",
+      address: "68/1 Soi Langsuan, Ploenchit Road, Lumpini, Bangkok 10330",
+      phone_number: "02 652 1700",
+      category: "indian"
+    },
+    {
+      name: "Ishikawa",
+      address: "5-37 Kagurazaka, Shinjuku-ku, Tokyo",
+      phone_number: "+81 3-5225-0173",
+      category: "japanese"
+    }
+  ]
+  
+  restaurants.each do |attributes|
+    restaurant = Restaurant.create!(attributes)
+    puts "Created #{restaurant.name}"
+  end
