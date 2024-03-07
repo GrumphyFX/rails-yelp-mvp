8453dcbb965372e1586806e425c3c0d5b0d08ac8
Bart Przespolewski <150512708+GrumphyFX@users.noreply.github.com>
2024-03-07 09:31:18 +0100

    rails-yelp-mvp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,5 +2,31 @@ class RestaurantsController < ApplicationController
     def index
       @restaurants = Restaurant.all
     end
-    # other actions...
+
+    def show
+      @restaurant = Restaurant.find(params[:id])
+      @review = Review.new
+    end
+
+
+    def new
+      @restaurant = Restaurant.new
+    end
+
+    
+    def create
+      @restaurant = Restaurant.new(restaurant_params)
+      if @restaurant.save
+        redirect_to restaurant_path(@restaurant), notice: 'Restaurant was successfully created.'
+      else
+        render :new, alert: 'Restaurant was not created.'
+      end
+    end
+
+    private
+
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+    end
+    
   end
